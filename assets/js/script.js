d756c39b305f4aaf8bd7623ef3bcab47d92346e7
Mioler1 <graveer3759@mail.ru>
2022-08-30 17:19:36 +0300

    added section how_me_work, comments, our_work

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,6 +32,40 @@ function app(){
             filter(currentCategoty, cards);
         })
     })
+
+    const swiper = new Swiper('.swiper', {
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        coverflowEffect: {
+            rotate: 50,
+            stretch: 0,
+            depht: 100,
+            modifier: 1,
+            slideShadows: true,
+        },
+
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+      
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      
+        // And if we need scrollbar
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+      });
 }
 
 app()
